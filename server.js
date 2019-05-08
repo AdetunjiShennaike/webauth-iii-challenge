b63957f3782dfe8edba4ed0dd9cbeb6e8c5f64d2
Adetunji <ashennaike@gmail.com>
2019-05-08 18:32:43 -0400

    finished mvp, server running just have to perform test

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express');
-const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoute = require('./auth/authRoute');
+const userRoute = require('./routes/userRoute');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(cors({ credentials: true, origin: 'http://localhost:3000' })); //credentials connect from front and backend
+
+server.use('/api/auth', authRoute);
+server.use('/api/user', userRoute);
 
 module.exports = server;
