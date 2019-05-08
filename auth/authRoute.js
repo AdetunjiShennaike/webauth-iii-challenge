124b8182352dc230f883ab35e2c01b0ec442a4cc
Adetunji <ashennaike@gmail.com>
2019-05-08 18:12:50 -0400

    almost done with routes just doing some slight eiditng

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const Users = require('../data/models/userModel');
 
@@ -8,6 +9,23 @@ const Users = require('../data/models/userModel');
 const sendError = (sts, msg, res) => {
   res.status(sts).json({ error: `${msg}`});
 };
+
+
+//token function
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['standard', 'student']
+  }
+  const secret = process.env.secret
+  const options = { 
+    expiresIn: '20h'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
+
 
 router.post('/register', (req, res) => {
   const user = req.body
@@ -25,5 +43,8 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  
+
 })
+
+
+module.exports = router;
