664ad95c3c9c8b8afae1621664994969d4088945
Adetunji <ashennaike@gmail.com>
2019-05-08 18:28:15 -0400

    finished the routes, going to work on server file

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -43,6 +43,25 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  const { username, password } = req.body
+
+  Users
+  .getByUser({ username })
+  .then( user => {
+    if ( user && bcrypt.compareSync(password, user.password)) {
+      const token = makeToken(user)
+      res.status(200).json({
+        message: `Welcome ${user.username}`, 
+        token
+      })
+    }
+    else {
+      return sendError(401, 'try again', res)
+    }
+  })
+  .catch( err => {
+    return sendError(500, err, res);
+  })
 
 })
 
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const express = require('express');
+const express = require('express');
+const express = require('express');
+const express = require('express');
+const express = require('express');
+
+module.exports = server;
